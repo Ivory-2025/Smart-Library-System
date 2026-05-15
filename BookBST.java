2a1369b3b275552937bc0339ca4842cffa92a5c7
LEEPY <25006494@siswa.um.edu.my>
2026-05-16 00:43:45 +0800

    Complete insert and ins method for BookBST class

---
+++ BookBST.java
@@ -0,0 +1,30 @@
+public class BookBST {
+    private Book root;
+
+    public void insert(int isbn, String t, String a) {
+        root = ins(root, isbn, t, a);
+    }
+
+    private Book ins(Book r, int i, String t, String a) {
+        // If current node is null, create and insert new book
+        if (r == null) return new Book(i, t, a);
+
+        // If new ISBN is smaller, insert into left subtree
+        if (i < r.isbn) {
+            r.left = ins(r.left, i, t, a);
+
+        // If new ISBN is larger, insert into right subtree
+        } else if (i > r.isbn) {
+            r.right = ins(r.right, i, t, a);
+        }
+        return r;
+    }
+
+    public Book search(int i) {
+        return null;
+    }
+
+    private Book sea(Book r, int i) {
+        return null;
+    }
+}
